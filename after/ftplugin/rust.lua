bc35bae3e725f10baa07e9db69a474d86a33a750
Aida (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2025-02-13 15:05:54 +0100

    Made debug previous again not limited to buffer

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -4,18 +4,21 @@
 --   tools = {
 --   },
 -- }
+-- Right now, only things provided by rustaceanvim.
 -- Keybinds must be set with `buffer = true`, if you don't want the bindings to be permanent for all buffers once it's been loaded.
 vim.keymap.set('n', '<leader>us', function()
   vim.cmd.RustLsp { 'testables' }
 end, { desc = '[S]how testables ', buffer = true })
 
+-- For this one, we want to be able to run the previous test again, even from a different buffer. So, remove buffer=true. Side effect is you can re-run them if you closed all rust files.
 vim.keymap.set('n', '<leader>ua', function()
   vim.cmd.RustLsp { 'testables', bang = true }
-end, { desc = '[U]nittest previous [A]gain ', buffer = true })
+end, { desc = '[U]nittest previous [A]gain ' })
 
+-- Same as above. So, remove buffer=true. Side effect is you can re-run them if you closed all rust files.
 vim.keymap.set('n', '<leader>da', function()
   vim.cmd.RustLsp { 'debuggables', bang = true }
-end, { desc = '[D]ebug previous [A]gain ', buffer = true })
+end, { desc = '[D]ebug previous [A]gain ' })
 
 vim.keymap.set('n', '<leader>pe', function()
   vim.cmd.RustLsp { 'explainError', 'current' }

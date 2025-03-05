6fc3921cfdd823ec68c33a239c87e3919f93b88c
Aida (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2025-03-05 12:06:16 +0100

    Change debugger stepping into/out/continue functions, because arrows were a little eh

--- lua/custom/plugins/nvim-dap.lua
+++ lua/custom/plugins/nvim-dap.lua
@@ -9,16 +9,16 @@ return {
       -- Conditionally set keybinds, only during DAP session.
       -- Inspired by https://github.com/mfussenegger/dotfiles/blob/e7abb9a13f8fb3075704ed703dd973ecf3502cc3/vim/.config/nvim/lua/me/dap.lua#L64-L75
       dap.listeners.after.event_initialized['custom.dap.keys'] = function()
-        vim.keymap.set('n', '<Enter>', dap.continue, { desc = 'Debug ▶️' })
-        vim.keymap.set('n', '<right>', dap.step_over, { desc = 'Debug ' })
-        vim.keymap.set('n', '<down>', dap.step_into, { desc = 'Debug ' })
-        vim.keymap.set('n', '<up>', dap.step_out, { desc = 'Debug ' })
+        vim.keymap.set('n', '<Tab>', dap.continue, { desc = 'Debug ▶️' })
+        vim.keymap.set('n', '<Enter>', dap.step_over, { desc = 'Debug ' })
+        vim.keymap.set('n', ';', dap.step_into, { desc = 'Debug ' })
+        vim.keymap.set('n', '<BS>', dap.step_out, { desc = 'Debug ' })
       end
       local reset_keys = function()
-        pcall(vim.keymap.del, 'n', '<down>')
-        pcall(vim.keymap.del, 'n', '<up>')
-        pcall(vim.keymap.del, 'n', '<right>')
+        pcall(vim.keymap.del, 'n', '<Tab>')
         pcall(vim.keymap.del, 'n', '<Enter>')
+        pcall(vim.keymap.del, 'n', ';')
+        pcall(vim.keymap.del, 'n', '<BS>')
       end
       dap.listeners.after.event_terminated['me.dap.keys'] = reset_keys
       dap.listeners.after.disconnected['me.dap.keys'] = reset_keys

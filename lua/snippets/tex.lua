28f20c1d1db23802376b959098d80aa7f3416178
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-05-26 16:15:18 +0200

    snippet(latex) Add table snippet for fixed-width 2

--- lua/snippets/tex.lua
+++ lua/snippets/tex.lua
@@ -18,4 +18,24 @@ add(s('equation', {
   t { '', '\\end{equation}' },
 }))
 
+add(s('table2w', {
+  -- 'ht' prefers placing it here, but otherwise the top of the page is also OK.
+  -- 'H' places it exactly here.
+  t { '\\begin{table}[ht]', '\\centering', '\\begin{tabular}{lc}\\toprule', '    & ' },
+  i(1),
+  t { ' & ' },
+  -- Can add a \cmidrule for if you you have a multi-layer heading and need a small center line here too
+  i(2),
+  t { ' \\\\', '\\midrule', ' ' },
+  i(3),
+  t { ' & ' },
+  i(4),
+  t { ' & ' },
+  i(5),
+  t { ' \\\\', '\\bottomrule', '\\end{tabular}', '\\end{table}' },
+}))
+
+-- There is a rather complex script that automatically does latex tables.
+-- Dynamic latex table, taken from https://github.com/L3MON4D3/LuaSnip/wiki/Misc#dynamicnode-with-user-input
+
 return snippets

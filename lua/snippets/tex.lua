c2465391dda5a27918243dc75d71e0bf3480bbd8
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-05-26 16:26:11 +0200

    fix(snippet) latex tables should have caption and label

--- lua/snippets/tex.lua
+++ lua/snippets/tex.lua
@@ -21,17 +21,44 @@ add(s('equation', {
 add(s('table2w', {
   -- 'ht' prefers placing it here, but otherwise the top of the page is also OK.
   -- 'H' places it exactly here.
-  t { '\\begin{table}[ht]', '\\centering', '\\begin{tabular}{lc}\\toprule', '    & ' },
+  t { '\\begin{table}[ht]', '\\centering', '\\label{tab:' },
   i(1),
+  t { '}', '\\caption{' },
+  i(2),
+  t { '}', '\\begin{tabular}{lc}\\toprule', '    & ' },
+  i(3),
   t { ' & ' },
   -- Can add a \cmidrule for if you you have a multi-layer heading and need a small center line here too
+  i(4),
+  t { ' \\\\', '\\midrule', '% copy-paste the line under here for how many lines you need.', ' ' },
+  i(5),
+  t { ' & ' },
+  i(6),
+  t { ' & ' },
+  i(7),
+  t { ' \\\\', '\\bottomrule', '\\end{tabular}', '\\end{table}' },
+}))
+
+add(s('table3w', {
+  t { '\\begin{table}[ht]', '\\centering', '\\label{tab:' },
+  i(1),
+  t { '}', '\\caption{' },
   i(2),
-  t { ' \\\\', '\\midrule', ' ' },
+  t { '}', '\\begin{tabular}{lcs}\\toprule', '    & ' },
   i(3),
   t { ' & ' },
   i(4),
   t { ' & ' },
+  -- Can add a \cmidrule for if you you have a multi-layer heading and need a small center line here too
   i(5),
+  t { ' \\\\', '\\midrule', '% copy-paste the line under here for how many lines you need.', ' ' },
+  i(6),
+  t { ' & ' },
+  i(7),
+  t { ' & ' },
+  i(8),
+  t { ' & ' },
+  i(9),
   t { ' \\\\', '\\bottomrule', '\\end{tabular}', '\\end{table}' },
 }))
 

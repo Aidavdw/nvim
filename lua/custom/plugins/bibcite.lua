793b116f2dd0983ba61dd187d11243b3b9141d7f
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-06-04 08:20:25 +0200

    fix(bibcite) Lazily load keybinds

--- lua/custom/plugins/bibcite.lua
+++ lua/custom/plugins/bibcite.lua
@@ -3,12 +3,16 @@ return {
   name = 'bibcite', -- optional name if it's not in a Git repo
   -- Running these commands triggers lazy load. They are still auto-completed.
   cmd = { 'CiteDebug', 'CiteOpen', 'CiteInsert', 'CitePeek', 'CiteNote' },
-  -- TODO: Figure out a way to trigger lazy load with keybinds. https://lazy.folke.io/spec/lazy_loading#%EF%B8%8F-lazy-key-mappings
-  config = function()
-    require('bibcite').setup {
-      bibtex_path = vim.fn.expand '/alt/code/bibliography/thesis.bib',
-      pdf_dir = vim.fn.expand '/alt/code/bibliography/files',
-      notes_dir = vim.fn.expand '~/Desktop/fake_bib_notes',
-    }
-  end,
+  -- Hitting these keybinds triggers lazy-load. They still show up in which-keys.
+  keys = {
+    { '<leader>ci', ':CiteInsert<CR>', desc = 'Insert citation' },
+    { '<leader>cp', ':CitePeek<CR>', desc = 'Peek citation info' },
+    { '<leader>co', ':CiteOpen<CR>', desc = 'Open citation file' },
+    { '<leader>cn', ':CiteNote<CR>', desc = 'Open citation note' },
+  },
+  opts = {
+    bibtex_path = vim.fn.expand '/alt/code/bibliography/thesis.bib',
+    pdf_dir = vim.fn.expand '/alt/code/bibliography/files',
+    notes_dir = vim.fn.expand '~/notes/literature-notes',
+  },
 }

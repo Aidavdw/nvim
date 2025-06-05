1d68b49e3eb486a88761f9c11f33748faf5e44d9
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-06-05 21:29:28 +0200

    fix(snippets) Tex tables one too long

--- lua/snippets/tex.lua
+++ lua/snippets/tex.lua
@@ -26,22 +26,21 @@ add(s('table2w', {
   i(1),
   t { '}', '\\caption{' },
   i(2),
-  t { '}', '\\begin{tabular}{lc}\\toprule', '    & ' },
+  t { '}', '\\begin{tabular}{lcc}\\toprule', ' ' },
   i(3),
   t { ' & ' },
-  -- Can add a \cmidrule for if you you have a multi-layer heading and need a small center line here too
   i(4),
   t { ' \\\\', '\\midrule', '% copy-paste the line under here for how many lines you need.', ' ' },
   i(5),
   t { ' & ' },
   i(6),
-  t { ' & ' },
-  i(7),
   t { ' \\\\', '\\bottomrule', '\\end{tabular}', '\\end{table}' },
 }))
 
 -- A table of 3 columns wide
 add(s('table3w', {
+  -- 'ht' prefers placing it here, but otherwise the top of the page is also OK.
+  -- 'H' places it exactly here.
   t { '\\begin{table}[ht]', '\\centering', '\\label{tab:' },
   i(1),
   t { '}', '\\caption{' },
@@ -49,18 +48,14 @@ add(s('table3w', {
   t { '}', '\\begin{tabular}{lcc}\\toprule', '    & ' },
   i(3),
   t { ' & ' },
-  i(4),
-  t { ' & ' },
   -- Can add a \cmidrule for if you you have a multi-layer heading and need a small center line here too
-  i(5),
+  i(4),
   t { ' \\\\', '\\midrule', '% copy-paste the line under here for how many lines you need.', ' ' },
+  i(5),
+  t { ' & ' },
   i(6),
   t { ' & ' },
   i(7),
-  t { ' & ' },
-  i(8),
-  t { ' & ' },
-  i(9),
   t { ' \\\\', '\\bottomrule', '\\end{tabular}', '\\end{table}' },
 }))
 
@@ -76,8 +71,30 @@ add(s('table4w', {
   i(4),
   t { ' & ' },
   i(5),
+  t { ' \\\\', '\\midrule', '% copy-paste the line under here for how many lines you need.', ' ' },
+  i(6),
   t { ' & ' },
-  -- Can add a \cmidrule for if you you have a multi-layer heading and need a small center line here too
+  i(7),
+  t { ' & ' },
+  i(8),
+  t { ' & ' },
+  i(9),
+  t { ' \\\\', '\\bottomrule', '\\end{tabular}', '\\end{table}' },
+}))
+
+-- A table of 5 columns wide
+add(s('table5w', {
+  t { '\\begin{table}[ht]', '\\centering', '\\label{tab:' },
+  i(1),
+  t { '}', '\\caption{' },
+  i(2),
+  t { '}', '\\begin{tabular}{lcccc}\\toprule', '    & ' },
+  i(3),
+  t { ' & ' },
+  i(4),
+  t { ' & ' },
+  i(5),
+  t { ' & ' },
   i(6),
   t { ' \\\\', '\\midrule', '% copy-paste the line under here for how many lines you need.', ' ' },
   i(7),
@@ -148,5 +165,17 @@ add(s('subfigure2w', {
 }))
 -- There is a rather complex script that automatically does latex tables.
 -- Dynamic latex table, taken from https://github.com/L3MON4D3/LuaSnip/wiki/Misc#dynamicnode-with-user-input
+
+add(s('enumerate', {
+  t { '\begin{enumeration}', '  \\item ' },
+  i(1),
+  t { '', '\\end{enumerate}' },
+}))
+
+add(s('itemize', {
+  t { '\begin{itemize}', '  \\item ' },
+  i(1),
+  t { '', '\\end{itemize}' },
+}))
 
 return snippets

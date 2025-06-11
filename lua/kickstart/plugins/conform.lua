cf8705bcbd88e6f012fb5cabb5ce4ec5ebce461d
Aidavdw <39270007+Aidavdw@users.noreply.github.com>
2025-06-11 10:29:48 +0200

    Hotkey to disable autoformatting for this buffer

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
     cmd = { 'ConformInfo' },
     keys = {
       {
-        -- Propably not used as much, since autoformat on save. Still nice to have as a backup.
+        -- Probably not used as much, since autoformat on save. Still nice to have as a backup.
         '<leader>ba',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
@@ -13,10 +13,29 @@ return {
         mode = '',
         desc = '[B]uffer: Auto[F]ormat',
       },
+      {
+        '<leader>bd',
+        function()
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+          if vim.b.disable_autoformat then
+            vim.notify('Autoformat on save disabled for this buffer', vim.log.levels.INFO)
+          else
+            vim.notify('Autoformat on save enabled for this buffer', vim.log.levels.INFO)
+          end
+        end,
+        desc = '[B]uffer: toggle autoformat on [S]ave',
+      },
     },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Check if a variable is set for this specific buffer to *NOT* apply autoformattin.
+        -- In doing so, we can opt-out of autoformatting.
+        -- Note that this resets on every time you load the file, but it's a rare-enough occurrence that this should be pretty alright
+        if vim.b[bufnr].disable_autoformat then
+          return nil
+        end
+
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.

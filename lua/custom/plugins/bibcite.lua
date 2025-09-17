4aafd0752b4a769b2c59259d3452784db26536b6
Aida van de Wetering <aida@osphire.com>
2025-09-17 11:45:49 +0200

    move local bibcite

--- lua/custom/plugins/bibcite.lua
+++ lua/custom/plugins/bibcite.lua
@@ -1,14 +1,14 @@
 return {
-  dir = '/alt/code/bibcite.nvim/', -- path to your local plugin
-  name = 'bibcite', -- optional name if it's not in a Git repo
+  dir = '/home/aida/repos/bibcite.nvim/', -- path to your local plugin
+  name = 'bibcite',                       -- optional name if it's not in a Git repo
   -- Running these commands triggers lazy load. They are still auto-completed.
   cmd = { 'CiteDebug', 'CiteOpen', 'CiteInsert', 'CitePeek', 'CiteNote' },
   -- Hitting these keybinds triggers lazy-load. They still show up in which-keys.
   keys = {
     { '<leader>ci', ':CiteInsert<CR>', desc = 'Insert citation' },
-    { '<leader>cp', ':CitePeek<CR>', desc = 'Peek citation info' },
-    { '<leader>co', ':CiteOpen<CR>', desc = 'Open citation file' },
-    { '<leader>cn', ':CiteNote<CR>', desc = 'Open citation note' },
+    { '<leader>cp', ':CitePeek<CR>',   desc = 'Peek citation info' },
+    { '<leader>co', ':CiteOpen<CR>',   desc = 'Open citation file' },
+    { '<leader>cn', ':CiteNote<CR>',   desc = 'Open citation note' },
   },
   opts = {
     bibtex_path = vim.fn.expand '/alt/code/bibliography/thesis.bib',
